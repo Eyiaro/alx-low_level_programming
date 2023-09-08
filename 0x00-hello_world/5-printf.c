bb77c4782c5dd400d633d38cb959b50c018ab26f
Eyiday <eyidaythedon@gmail.com>
2023-09-07 21:39:40 -0700

    git

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- * main 
- *
+ * main - print the string in the printf function 
  * description: Write a C program that prints exactly with proper grammar,
  * but the outcome is a piece of art,
  * return 0
