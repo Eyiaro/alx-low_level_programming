f49dc45f56cba4832040984c478519602266e311
Eyiday <eyidaythedon@gmail.com>
2023-09-26 03:08:44 -0700

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,11 +4,10 @@
  *
  * description: Write a C program that prints exactly with proper grammar,
  * but the outcome is a piece of art,
- * return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 }
